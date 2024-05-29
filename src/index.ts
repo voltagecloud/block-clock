e725315b62eb4104a410e37a1822b87ddda699d5
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2024-05-29 13:54:26 -0400

    maintain aspect ratio of clock at any container size

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import { LitElement, css, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
-import litLogo from "./assets/lit.svg";
-import viteLogo from "/vite.svg";
 
 /**
  * An example element.
@@ -30,29 +28,14 @@ export class Blockclock extends LitElement {
   darkMode = true;
 
   render() {
-    return html`
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src=${viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://lit.dev" target="_blank">
-          <img src=${litLogo} class="logo lit" alt="Lit logo" />
-        </a>
-        <p>hello hello</p>
-      </div>
-      <slot></slot>
-      <div class="card">
-        <button @click=${this._onClick} part="button">
-          count is ${this.count}
-        </button>
-      </div>
-      <p class="read-the-docs">${this.docsHint}</p>
-    `;
+    return html` <div class="square">
+      <div class="circle"></div>
+    </div>`;
   }
 
-  private _onClick() {
-    this.count++;
-  }
+  // private _onClick() {
+  //   this.count++;
+  // }
 
   static styles = css`
     :host {
@@ -62,59 +45,32 @@ export class Blockclock extends LitElement {
       text-align: center;
     }
 
-    .logo {
+    div.square {
+      height: 200px;
+      width: 200px;
+      background-color: red;
+      resize: both;
+      display: block;
+      overflow: hidden;
+    }
+
+    div.circle {
+      background-color: green;
+      max-height: 100%;
+      border-radius: 50%;
+      aspect-ratio: 1/1;
+    }
+    /* .logo {
       height: 6em;
       padding: 1.5em;
       will-change: filter;
       transition: filter 300ms;
-    }
-    .logo:hover {
-      filter: drop-shadow(0 0 2em #646cffaa);
-    }
-    .logo.lit:hover {
-      filter: drop-shadow(0 0 2em #325cffaa);
-    }
+    } */
 
-    .card {
-      padding: 2em;
-    }
-
-    .read-the-docs {
-      color: #888;
-    }
-
-    ::slotted(h1) {
+    /* ::slotted(h1) {
       font-size: 3.2em;
       line-height: 1.1;
-    }
-
-    a {
-      font-weight: 500;
-      color: #646cff;
-      text-decoration: inherit;
-    }
-    a:hover {
-      color: #535bf2;
-    }
-
-    button {
-      border-radius: 8px;
-      border: 1px solid transparent;
-      padding: 0.6em 1.2em;
-      font-size: 1em;
-      font-weight: 500;
-      font-family: inherit;
-      background-color: #1a1a1a;
-      cursor: pointer;
-      transition: border-color 0.25s;
-    }
-    button:hover {
-      border-color: #646cff;
-    }
-    button:focus,
-    button:focus-visible {
-      outline: 4px auto -webkit-focus-ring-color;
-    }
+    } */
 
     @media (prefers-color-scheme: light) {
       a:hover {
