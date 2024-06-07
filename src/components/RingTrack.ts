3455560706850e02c65e63b3d3f5ca6691cd39df
Lightning K0ala <lightning.k0ala@protonmail.com>
2024-06-07 23:17:54 +0100

    ring points

--- src/components/RingTrack.ts
+++ src/components/RingTrack.ts
@@ -1,4 +1,4 @@
-import { html } from "lit";
+import { html, svg } from "lit";
 import { styleMap } from "lit/directives/style-map.js";
 
 export interface RingTrackProps {
@@ -7,6 +7,11 @@ export interface RingTrackProps {
 }
 
 export const RingTrack = ({ ringWidth, size }: RingTrackProps) => {
+  const pointScale = 0.25;
+  const pointArrays = [0, 45, 90, 135, 180, 225, 270, 315].map((angle) =>
+    convertAngleToCxCyAroundCircle(angle, ringWidth)
+  );
+  const pointSize = ringWidth * size * pointScale;
   return html`
     <svg viewBox="0 0 100 100" style=${styleMap(styles.svg)}>
       <circle
@@ -16,6 +21,16 @@ export const RingTrack = ({ ringWidth, size }: RingTrackProps) => {
         r=${50 - ringWidth / 2}
         stroke-width="${ringWidth * size}"
       />
+      ${pointArrays.map(
+        ({ cx, cy }) => svg`
+          <circle
+            style=${styleMap(styles.svgPoint)}
+            cx=${cx}
+            cy=${cy}
+            r=${pointSize}
+          />
+        `
+      )}
     </svg>
   `;
 };
@@ -28,10 +43,19 @@ const styles = {
     top: "0",
     left: "0",
     zIndex: 1,
-    transform: "rotate(-90deg)",
     fill: "none",
   },
   svgCircle: {
     stroke: "var(--ring-track-color, #333)",
   },
+  svgPoint: {
+    fill: "var(--ring-point-color, #333)",
+  },
 };
+
+function convertAngleToCxCyAroundCircle(angle: number, ringWidth: number) {
+  const radians = (angle * Math.PI) / 180;
+  const cx = 50 + Math.sin(radians) * (50 - ringWidth * 0.5);
+  const cy = 50 - Math.cos(radians) * (50 - ringWidth * 0.5);
+  return { cx, cy };
+}
